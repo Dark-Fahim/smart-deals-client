d2e06bcf208c6605108ba760e7b41e6c47d6b3c9
Dark-Fahim <itsmunna1066@gmail.com>
2025-11-03 20:52:36 +0600

    config changed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKWKHNdcl3KgcHcxEIqWsXeG9-RZ3ZuhA",
-  authDomain: "smart-deals-8ed81.firebaseapp.com",
-  projectId: "smart-deals-8ed81",
-  storageBucket: "smart-deals-8ed81.firebasestorage.app",
-  messagingSenderId: "367283112349",
-  appId: "1:367283112349:web:8f10f7cbf2ff29aa604774"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
